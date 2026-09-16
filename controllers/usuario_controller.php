d04146b38fe4ffa47f4663334527b7e43638612e
icoooh <victorhugosalomao2009@gmail.com>
2026-09-16 14:37:24 -0300

    correção

--- controllers/usuario_controller.php
+++ controllers/usuario_controller.php
@@ -2,10 +2,10 @@
 
 function usuarioController(){
     echo "6. Controller recebeu a requisição.<br>";
-    $usuarios = usuarioservice();
+    $usuarios = usuarioService();
     echo "8. Controller recebeu os dados do Service.<br>";
-    echo "Usuarios encontrados:<br>";
-    foreach ($usuaios as $usuario) {
+    echo "Usuários encontrados:<br>";
+    foreach ($usuarios as $usuario) {
         echo "- " . $usuario . "<br>";
     }
 }
